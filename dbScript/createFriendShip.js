a56c91d8f200ccaadce8999fce0d2dfdc8c369ab
MiKhai37 <mi.khai.thai@gmail.com>
2021-10-29 08:23:12 +0200

    Implement script to create one friendship between two random users

--- dbScript/createFriendShip.js
+++ dbScript/createFriendShip.js
@@ -2,17 +2,12 @@
 require('dotenv').config({ path: '../.env' });
 
 const userArgs = process.argv.slice(2);
-const nbRelations = userArgs[0] || 100
+const nbFriendships = userArgs[0] || 100
 
 const async = require('async');
-const faker = require('faker');
-//faker.seed(37);
-const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const UserModel = require('./models/user');
-const PostModel = require('./models/post');
-const CommentModel = require('./models/comment');
 
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -20,4 +15,47 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-console.log('Database connection opened')
+console.log('Database connection opened');
+
+const findRandomUserPair = async () => {
+  const users = await UserModel.find({}).exec();
+  const random = Math.floor(Math.random() * users.length);
+  const firstUser = users[random];
+  console.log('First User: ', firstUser._id)
+
+  const usersWithoutFirst = users.filter(user => user._id !== firstUser._id);
+  const randomBis = Math.floor(Math.random() * usersWithoutFirst.length);
+  const secondUser = usersWithoutFirst[randomBis]
+  console.log('Second User: ', secondUser._id)
+
+  UserModel
+    .findOne({ _id: firstUser._id })
+    .then(user => {
+      const friends = user.friends;
+      user.friends = [...friends, secondUser._id];
+      user.save(function(err, user) {
+        if (err) {
+          console.error(err);
+          return;
+        };
+        console.log(user);
+      });
+    });
+
+  UserModel
+    .findOne({ _id: secondUser._id })
+    .then(user => {
+      const friends = user.friends;
+      user.friends = [...friends, firstUser._id]
+      user.save(function(err, user) {
+        if (err) {
+          console.error(err);
+          return;
+        };
+        console.log(user);
+      });
+    });
+
+}
+
+findRandomUserPair()
