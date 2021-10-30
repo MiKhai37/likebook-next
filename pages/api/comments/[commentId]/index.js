e026d9df3c983b2613f816e8ed595d687608d4c8
MiKhai37 <mi.khai.thai@gmail.com>
2021-10-30 17:01:19 +0200

    small edit

--- pages/api/comments/[commentId]/index.js
+++ pages/api/comments/[commentId]/index.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   };
 
   if (req.method === 'DELETE') {
-    comment.remove();
+    await comment.remove();
     return res.status(200).json({ message: "Comment deleted" });
   };
 
