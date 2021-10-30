2493b9176e9712044957f848ec3980b5583631ed
MiKhai37 <mi.khai.thai@gmail.com>
2021-10-30 16:36:55 +0200

    Add and admin dashborad

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import useSWR from 'swr';
+import { Typography } from 'antd';
+
+const { Title } = Typography
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Admin = () => {
+  const { data: users, error: usersError } = useSWR("/api/admin/users", fetcher);
+  const { data: posts, error: postsError } = useSWR("/api/admin/posts", fetcher);
+  const { data: comments, error: commentsError } = useSWR("/api/admin/comments", fetcher);
+
   return (
     <>
-      
+      <Title level={1} >Admin Dashboard</Title>
+      <Title level={3} >Users</Title>
+      <p>{users?.data?.users.length}</p>
+      <Title level={3} >Posts</Title>
+      <p>{posts?.data?.posts.length}</p>
+      <Title level={3} >Comments</Title>
+      <p>{comments?.data?.comments.length}</p>
     </>
   );
 };
