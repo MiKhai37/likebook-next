3d7cfb52882f40aa1845a43f8cb09dfe812f176c
MiKhai37 <mi.khai.thai@gmail.com>
2021-10-30 16:50:49 +0200

    Ignore admin role in API for dev env

--- pages/api/admin/users.js
+++ pages/api/admin/users.js
@@ -4,8 +4,10 @@ import { getLoginSession } from "../../../lib/auth/auth";
 export default async function handler(req, res) {
 
   const session = await getLoginSession(req);
-  if (!session.admin) {
-    res.send({ error: 'You must be admin to use this.' });
+  if (process.env.NODE_ENV !== 'development') {
+    if (!session.admin) {
+      res.send({ error: 'You must be admin to use this.' });
+    };
   };
   
   // List of all users
